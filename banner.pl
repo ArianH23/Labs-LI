e149ee34d8658a986fb5d1921a8f705df936a035
ArianH23 <arian.huapaya@gmail.com>
2020-06-02 13:46:41 +0200

    Banner and gangster libs fixed

--- banner.pl
+++ banner.pl
@@ -52,7 +52,7 @@ symbolicOutput(0).  % set to 1 to see symbolic output only; 0 otherwise.
 %% .555...111.
 %% .555...111.
       	 
-:-include(input4).
+:-include(input1).
 
 %%%%%% Some helpful definitions to make the code cleaner:
 piece(P):-                  pieces(L), member([P,_,_],L).
@@ -61,7 +61,7 @@ widthBanner(W):-            banner(B), member(L,B), length(L,W),!.
 heightBanner(H):-           banner(B), length(B,H), !.
 contentsCellBanner(X,Y,C):- cell(X,Y), banner(B), heightBanner(H), Y1 is H-Y+1, nth1(Y1,B,L), nth1(X,L,C).
 cell(X,Y):-                 widthBanner(W), heightBanner(H), between(1,W,X), between(1,H,Y).
-numPieces(N):-              nth1(1,pieces(_),L),length(L, N),!.
+numPieces(N):-              pieces(L),length(L, N),!.
 
 getCells(Xstart, Ystart, X, Y, SizeW, SizeH):-
     cell(Xstart, Ystart),
@@ -90,7 +90,7 @@ satVariable(pieceStarts(P,X,Y)):- piece(P),cell(X,Y).                           
 satVariable(used(P)):- piece(P).                                                 % means: "piece P is used"
 
 
-writeClauses(infinite):- !, writeClauses(9),!.
+writeClauses(infinite):- !, numPieces(N), writeClauses(N),!.
 writeClauses(K):-
     eachPieceAMO,
     eachCellOnePiece,
@@ -246,59 +246,13 @@ treatResult(10,_):- %   shell('cat model',_),
 treatResult(_,_):- write('cnf input error. Wrote something strange in your cnf?'), nl,nl, halt.
     
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Everything below is given as a standard library, reusable for solving 
-%    with SAT many different problems.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Express that Var is equivalent to the disjunction of Lits.%
-expressOr( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> or('), write(Lits), write(')'), nl, !. 
-expressOr( Var, Lits ):- member(Lit,Lits), negate(Lit,NLit), writeClause([ NLit, Var ]), fail.
-expressOr( Var, Lits ):- negate(Var,NVar), writeClause([ NVar | Lits ]),!.
-% Express that Var is equivalent to the conjunction of Lits:
-expressAnd( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> and('), write(Lits), write(')'), nl, !. 
-expressAnd( Var, Lits):- member(Lit,Lits), negate(Var,NVar), writeClause([ NVar, Lit ]), fail.
-expressAnd( Var, Lits):- findall(NLit, (member(Lit,Lits), negate(Lit,NLit)), NLits), writeClause([ Var | NLits]), !.
-%%%%%% Cardinality constraints on arbitrary sets of literals Lits:
-exactly(K,Lits):- symbolicOutput(1), write( exactly(K,Lits) ), nl, !.
-exactly(K,Lits):- atLeast(K,Lits), atMost(K,Lits),!.
-atMost(K,Lits):- symbolicOutput(1), write( atMost(K,Lits) ), nl, !.
-atMost(K,Lits):-   % l1+...+ln <= k:  in all subsets of size k+1, at least one is false:
-    negateAll(Lits,NLits),
-    K1 is K+1,    subsetOfSize(K1,NLits,Clause), writeClause(Clause),fail.
-atMost(_,_).
-atLeast(K,Lits):- symbolicOutput(1), write( atLeast(K,Lits) ), nl, !.
-atLeast(K,Lits):-  % l1+...+ln >= k: in all subsets of size n-k+1, at least one is true:
-    length(Lits,N),
-    K1 is N-K+1,  subsetOfSize(K1, Lits,Clause), writeClause(Clause),fail.
-atLeast(_,_).
-negateAll( [], [] ).
-negateAll( [Lit|Lits], [NLit|NLits] ):- negate(Lit,NLit), negateAll( Lits, NLits ),!.
-negate( -Var,  Var):-!.
-negate(  Var, -Var):-!.
-subsetOfSize(0,_,[]):-!.
-subsetOfSize(N,[X|L],[X|S]):- N1 is N-1, length(L,Leng), Leng>=N1, subsetOfSize(N1,L,S).
-subsetOfSize(N,[_|L],   S ):-            length(L,Leng), Leng>=N,  subsetOfSize( N,L,S).
-%%%%%% main:
-main:-  symbolicOutput(1), !, writeClauses, halt.   % print the clauses in symbolic form and halt
-main:-  initClauseGeneration,
-tell(clauses), writeClauses, told,          % generate the (numeric) SAT clauses and call the solver
-tell(header),  writeHeader,  told,
-numVars(N), numClauses(C),
-write('Generated '), write(C), write(' clauses over '), write(N), write(' variables. '),nl,
-shell('cat header clauses > infile.cnf',_),
-write('Calling solver....'), nl,
-shell('picosat -v -o model infile.cnf', Result),  % if sat: Result=10; if unsat: Result=20.
-    treatResult(Result),!.
-treatResult(20):- write('Unsatisfiable'), nl, halt.
-treatResult(10):- write('Solution found: '), nl, see(model), symbolicModel(M), seen, displaySol(M), nl,nl,halt.
-treatResult( _):- write('cnf input error. Wrote anything strange in your cnf?'), nl,nl, halt.
-    
 initClauseGeneration:-  %initialize all info about variables and clauses:
-    retractall(numClauses(   _)),
-    retractall(numVars(      _)),
-    retractall(varNumber(_,_,_)),
-    assert(numClauses( 0 )),
-    assert(numVars(    0 )),     !.
+	retractall(numClauses(   _)),
+	retractall(numVars(      _)),
+	retractall(varNumber(_,_,_)),
+	assert(numClauses( 0 )),
+	assert(numVars(    0 )),     !.
+
 writeClause([]):- symbolicOutput(1),!, nl.
 writeClause([]):- countClause, write(0), nl.
 writeClause([Lit|C]):- w(Lit), writeClause(C),!.
@@ -307,14 +261,18 @@ w( Var):- symbolicOutput(1), satVariable(Var), write( Var), write(' '),!.
 w(-Var):- satVariable(Var),  var2num(Var,N),   write(-), write(N), write(' '),!.
 w( Var):- satVariable(Var),  var2num(Var,N),             write(N), write(' '),!.
 w( Lit):- told, write('ERROR: generating clause with undeclared variable in literal '), write(Lit), nl,nl, halt.
+
+
 % given the symbolic variable V, find its variable number N in the SAT solver:
-:-dynamic(varNumber / 3).
 var2num(V,N):- hash_term(V,Key), existsOrCreate(V,Key,N),!.
 existsOrCreate(V,Key,N):- varNumber(Key,V,N),!.                            % V already existed with num N
 existsOrCreate(V,Key,N):- newVarNumber(N), assert(varNumber(Key,V,N)), !.  % otherwise, introduce new N for V
+
 writeHeader:- numVars(N),numClauses(C), write('p cnf '),write(N), write(' '),write(C),nl.
+
 countClause:-     retract( numClauses(N0) ), N is N0+1, assert( numClauses(N) ),!.
 newVarNumber(N):- retract( numVars(   N0) ), N is N0+1, assert(    numVars(N) ),!.
+
 % Getting the symbolic model M from the output file:
 symbolicModel(M):- get_code(Char), readWord(Char,W), symbolicModel(M1), addIfPositiveInt(W,M1,M),!.
 symbolicModel([]).
@@ -325,4 +283,47 @@ readWord(115,W):- repeat, get_code(Ch), member(Ch,[-1,10]), !, get_code(Ch1), re
 readWord(-1,_):-!, fail. %end of file
 readWord(C,[]):- member(C,[10,32]), !. % newline or white space marks end of word
 readWord(Char,[Char|W]):- get_code(Char1), readWord(Char1,W), !.
-%========================================================================================
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Everything below is given as a standard library, reusable for solving 
+%    with SAT many different problems.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Express that Var is equivalent to the disjunction of Lits:
+expressOr( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> or('), write(Lits), write(')'), nl, !. 
+expressOr( Var, Lits ):- member(Lit,Lits), negate(Lit,NLit), writeClause([ NLit, Var ]), fail.
+expressOr( Var, Lits ):- negate(Var,NVar), writeClause([ NVar | Lits ]),!.
+
+% Express that Var is equivalent to the conjunction of Lits:
+expressAnd( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> and('), write(Lits), write(')'), nl, !. 
+expressAnd( Var, Lits):- member(Lit,Lits), negate(Var,NVar), writeClause([ NVar, Lit ]), fail.
+expressAnd( Var, Lits):- findall(NLit, (member(Lit,Lits), negate(Lit,NLit)), NLits), writeClause([ Var | NLits]), !.
+
+
+%%%%%% Cardinality constraints on arbitrary sets of literals Lits:
+
+exactly(K,Lits):- symbolicOutput(1), write( exactly(K,Lits) ), nl, !.
+exactly(K,Lits):- atLeast(K,Lits), atMost(K,Lits),!.
+
+atMost(K,Lits):- symbolicOutput(1), write( atMost(K,Lits) ), nl, !.
+atMost(K,Lits):-   % l1+...+ln <= k:  in all subsets of size k+1, at least one is false:
+	negateAll(Lits,NLits),
+	K1 is K+1,    subsetOfSize(K1,NLits,Clause), writeClause(Clause),fail.
+atMost(_,_).
+
+atLeast(K,Lits):- symbolicOutput(1), write( atLeast(K,Lits) ), nl, !.
+atLeast(K,Lits):-  % l1+...+ln >= k: in all subsets of size n-k+1, at least one is true:
+	length(Lits,N),
+	K1 is N-K+1,  subsetOfSize(K1, Lits,Clause), writeClause(Clause),fail.
+atLeast(_,_).
+
+negateAll( [], [] ).
+negateAll( [Lit|Lits], [NLit|NLits] ):- negate(Lit,NLit), negateAll( Lits, NLits ),!.
+
+negate( -Var,  Var):-!.
+negate(  Var, -Var):-!.
+
+subsetOfSize(0,_,[]):-!.
+subsetOfSize(N,[X|L],[X|S]):- N1 is N-1, length(L,Leng), Leng>=N1, subsetOfSize(N1,L,S).
+subsetOfSize(N,[_|L],   S ):-            length(L,Leng), Leng>=N,  subsetOfSize( N,L,S).
+
--- gangsters.pl
+++ gangsters.pl
@@ -74,17 +74,18 @@ writeClauses(K):-
     menysDeKHoresSeguides(K),
     true,!.
 writeClauses(_):- told, nl, write('writeClauses failed!'), nl,nl, halt.
+
 defineGH :-
     gangster(G), hour(H),task(T),
     writeClause([-does(G,T,H), gh(G,H)]),
     fail.
 defineGH.
 
-
 menysDeKHoresSeguides(K):- 
     findall(N, between(1, 72, N), L), 
-    gangster(G), append([_,LK,_], L), 
-    append([_,LK,_], L), length(LK, K1), K1 is K+1,
+    gangster(G),
+    append([_,LK,_], L), 
+    length(LK, K1), K1 is K+1,
     findall(-gh(G,H), (hour(H), member(H, LK)), Hores), 
     atLeast(1, Hores),fail.
 menysDeKHoresSeguides(_).
@@ -147,6 +148,7 @@ algunGangsterTreballaConsecutivament(K,M):-
 
 gangsterTreballaConsecutivament(G,K,M):-
     llistaHoresTreballades(G,M,L),hihaSubllistaHoresConsecutives(K,L).
+
 llistaHoresTreballades(G,M,L) :- findall(H, member(gh(G,H),M), L).
 hihaSubllistaHoresConsecutives(K,L):-
     append([_,LK,_],L),length(LK,K),horesConsecutives(LK).
@@ -194,59 +196,13 @@ treatResult(10,_):- %   shell('cat model',_),
 treatResult(_,_):- write('cnf input error. Wrote something strange in your cnf?'), nl,nl, halt.
     
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Everything below is given as a standard library, reusable for solving 
-%    with SAT many different problems.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Express that Var is equivalent to the disjunction of Lits.%
-expressOr( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> or('), write(Lits), write(')'), nl, !. 
-expressOr( Var, Lits ):- member(Lit,Lits), negate(Lit,NLit), writeClause([ NLit, Var ]), fail.
-expressOr( Var, Lits ):- negate(Var,NVar), writeClause([ NVar | Lits ]),!.
-% Express that Var is equivalent to the conjunction of Lits:
-expressAnd( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> and('), write(Lits), write(')'), nl, !. 
-expressAnd( Var, Lits):- member(Lit,Lits), negate(Var,NVar), writeClause([ NVar, Lit ]), fail.
-expressAnd( Var, Lits):- findall(NLit, (member(Lit,Lits), negate(Lit,NLit)), NLits), writeClause([ Var | NLits]), !.
-%%%%%% Cardinality constraints on arbitrary sets of literals Lits:
-exactly(K,Lits):- symbolicOutput(1), write( exactly(K,Lits) ), nl, !.
-exactly(K,Lits):- atLeast(K,Lits), atMost(K,Lits),!.
-atMost(K,Lits):- symbolicOutput(1), write( atMost(K,Lits) ), nl, !.
-atMost(K,Lits):-   % l1+...+ln <= k:  in all subsets of size k+1, at least one is false:
-    negateAll(Lits,NLits),
-    K1 is K+1,    subsetOfSize(K1,NLits,Clause), writeClause(Clause),fail.
-atMost(_,_).
-atLeast(K,Lits):- symbolicOutput(1), write( atLeast(K,Lits) ), nl, !.
-atLeast(K,Lits):-  % l1+...+ln >= k: in all subsets of size n-k+1, at least one is true:
-    length(Lits,N),
-    K1 is N-K+1,  subsetOfSize(K1, Lits,Clause), writeClause(Clause),fail.
-atLeast(_,_).
-negateAll( [], [] ).
-negateAll( [Lit|Lits], [NLit|NLits] ):- negate(Lit,NLit), negateAll( Lits, NLits ),!.
-negate( -Var,  Var):-!.
-negate(  Var, -Var):-!.
-subsetOfSize(0,_,[]):-!.
-subsetOfSize(N,[X|L],[X|S]):- N1 is N-1, length(L,Leng), Leng>=N1, subsetOfSize(N1,L,S).
-subsetOfSize(N,[_|L],   S ):-            length(L,Leng), Leng>=N,  subsetOfSize( N,L,S).
-%%%%%% main:
-main:-  symbolicOutput(1), !, writeClauses, halt.   % print the clauses in symbolic form and halt
-main:-  initClauseGeneration,
-tell(clauses), writeClauses, told,          % generate the (numeric) SAT clauses and call the solver
-tell(header),  writeHeader,  told,
-numVars(N), numClauses(C),
-write('Generated '), write(C), write(' clauses over '), write(N), write(' variables. '),nl,
-shell('cat header clauses > infile.cnf',_),
-write('Calling solver....'), nl,
-shell('picosat -v -o model infile.cnf', Result),  % if sat: Result=10; if unsat: Result=20.
-    treatResult(Result),!.
-treatResult(20):- write('Unsatisfiable'), nl, halt.
-treatResult(10):- write('Solution found: '), nl, see(model), symbolicModel(M), seen, displaySol(M), nl,nl,halt.
-treatResult( _):- write('cnf input error. Wrote anything strange in your cnf?'), nl,nl, halt.
-    
 initClauseGeneration:-  %initialize all info about variables and clauses:
-    retractall(numClauses(   _)),
-    retractall(numVars(      _)),
-    retractall(varNumber(_,_,_)),
-    assert(numClauses( 0 )),
-    assert(numVars(    0 )),     !.
+	retractall(numClauses(   _)),
+	retractall(numVars(      _)),
+	retractall(varNumber(_,_,_)),
+	assert(numClauses( 0 )),
+	assert(numVars(    0 )),     !.
+
 writeClause([]):- symbolicOutput(1),!, nl.
 writeClause([]):- countClause, write(0), nl.
 writeClause([Lit|C]):- w(Lit), writeClause(C),!.
@@ -255,14 +211,18 @@ w( Var):- symbolicOutput(1), satVariable(Var), write( Var), write(' '),!.
 w(-Var):- satVariable(Var),  var2num(Var,N),   write(-), write(N), write(' '),!.
 w( Var):- satVariable(Var),  var2num(Var,N),             write(N), write(' '),!.
 w( Lit):- told, write('ERROR: generating clause with undeclared variable in literal '), write(Lit), nl,nl, halt.
+
+
 % given the symbolic variable V, find its variable number N in the SAT solver:
-:-dynamic(varNumber / 3).
 var2num(V,N):- hash_term(V,Key), existsOrCreate(V,Key,N),!.
 existsOrCreate(V,Key,N):- varNumber(Key,V,N),!.                            % V already existed with num N
 existsOrCreate(V,Key,N):- newVarNumber(N), assert(varNumber(Key,V,N)), !.  % otherwise, introduce new N for V
+
 writeHeader:- numVars(N),numClauses(C), write('p cnf '),write(N), write(' '),write(C),nl.
+
 countClause:-     retract( numClauses(N0) ), N is N0+1, assert( numClauses(N) ),!.
 newVarNumber(N):- retract( numVars(   N0) ), N is N0+1, assert(    numVars(N) ),!.
+
 % Getting the symbolic model M from the output file:
 symbolicModel(M):- get_code(Char), readWord(Char,W), symbolicModel(M1), addIfPositiveInt(W,M1,M),!.
 symbolicModel([]).
@@ -273,4 +233,47 @@ readWord(115,W):- repeat, get_code(Ch), member(Ch,[-1,10]), !, get_code(Ch1), re
 readWord(-1,_):-!, fail. %end of file
 readWord(C,[]):- member(C,[10,32]), !. % newline or white space marks end of word
 readWord(Char,[Char|W]):- get_code(Char1), readWord(Char1,W), !.
-%========================================================================================
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Everything below is given as a standard library, reusable for solving 
+%    with SAT many different problems.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Express that Var is equivalent to the disjunction of Lits:
+expressOr( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> or('), write(Lits), write(')'), nl, !. 
+expressOr( Var, Lits ):- member(Lit,Lits), negate(Lit,NLit), writeClause([ NLit, Var ]), fail.
+expressOr( Var, Lits ):- negate(Var,NVar), writeClause([ NVar | Lits ]),!.
+
+% Express that Var is equivalent to the conjunction of Lits:
+expressAnd( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> and('), write(Lits), write(')'), nl, !. 
+expressAnd( Var, Lits):- member(Lit,Lits), negate(Var,NVar), writeClause([ NVar, Lit ]), fail.
+expressAnd( Var, Lits):- findall(NLit, (member(Lit,Lits), negate(Lit,NLit)), NLits), writeClause([ Var | NLits]), !.
+
+
+%%%%%% Cardinality constraints on arbitrary sets of literals Lits:
+
+exactly(K,Lits):- symbolicOutput(1), write( exactly(K,Lits) ), nl, !.
+exactly(K,Lits):- atLeast(K,Lits), atMost(K,Lits),!.
+
+atMost(K,Lits):- symbolicOutput(1), write( atMost(K,Lits) ), nl, !.
+atMost(K,Lits):-   % l1+...+ln <= k:  in all subsets of size k+1, at least one is false:
+	negateAll(Lits,NLits),
+	K1 is K+1,    subsetOfSize(K1,NLits,Clause), writeClause(Clause),fail.
+atMost(_,_).
+
+atLeast(K,Lits):- symbolicOutput(1), write( atLeast(K,Lits) ), nl, !.
+atLeast(K,Lits):-  % l1+...+ln >= k: in all subsets of size n-k+1, at least one is true:
+	length(Lits,N),
+	K1 is N-K+1,  subsetOfSize(K1, Lits,Clause), writeClause(Clause),fail.
+atLeast(_,_).
+
+negateAll( [], [] ).
+negateAll( [Lit|Lits], [NLit|NLits] ):- negate(Lit,NLit), negateAll( Lits, NLits ),!.
+
+negate( -Var,  Var):-!.
+negate(  Var, -Var):-!.
+
+subsetOfSize(0,_,[]):-!.
+subsetOfSize(N,[X|L],[X|S]):- N1 is N-1, length(L,Leng), Leng>=N1, subsetOfSize(N1,L,S).
+subsetOfSize(N,[_|L],   S ):-            length(L,Leng), Leng>=N,  subsetOfSize( N,L,S).
+
